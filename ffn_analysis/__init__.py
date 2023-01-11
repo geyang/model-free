82b66986069e2a9c5d19b7c55cdb99974a3306e0
Ge Yang <yangge1987@gmail.com>
2023-01-10 20:45:40 -0500

    remove the prefix

--- ffn_analysis/__init__.py
+++ ffn_analysis/__init__.py
@@ -1,9 +1,10 @@
-# from os.path import dirname, join
-#
-# from ml_logger import RUN, instr
-# from termcolor import colored
-#
-# assert instr  # single-entry for the instrumentation thunk factory
+from os.path import dirname, join
+
+from ml_logger import RUN, instr
+from termcolor import colored
+
+assert instr  # single-entry for the instrumentation thunk factory
+# note: Ge: commenting these out so that the default logger prefix is used, under each student's name
 # RUN.project = "model-free-rl"  # Specify the project name
 # # RUN.job_name = "{RUN.count}"
 # RUN.job_name += "/{job_counter}"
@@ -11,13 +12,3 @@
 # RUN.script_root = dirname(__file__)  # specify that this is the script root.
 # print(colored('set', 'blue'), colored("RUN.script_root", "yellow"), colored('to', 'blue'),
 #       RUN.script_root)
-#
-# AWS_REGIONS = [
-#     "ap-northeast-1", "ap-northeast-2", "ap-south-1", "ap-southeast-1",
-#     "ap-southeast-2", "eu-central-1", "eu-west-1", "sa-east-1", "us-east-1",
-#     "us-east-2", "us-west-1", "us-west-2", ]
-#
-# import yaml
-#
-# with open(join(dirname(__file__), "../ec2_setup/ec2_image_ids.yml"), 'r') as stream:
-#     IMAGE_IDS = yaml.load(stream, Loader=yaml.BaseLoader)
