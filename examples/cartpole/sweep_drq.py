a07912a54b3d951c88d721bcd71e30272868e648
Timur Garipov <timgaripov@gmail.com>
2023-01-16 12:20:05 -0500

    fix sweep order

--- examples/cartpole/sweep_drq.py
+++ examples/cartpole/sweep_drq.py
@@ -9,7 +9,6 @@ with Sweep(RUN, Args, Agent) as sweep:
     Args.from_pixels = False
 
     with sweep.product:
-        Args.seed = [100, 200, 300]
         with sweep.zip:
             Args.env_name = ['dmc:Cartpole-swingup-v1']
             Args.feature_dim = [50]
@@ -17,6 +16,7 @@ with Sweep(RUN, Args, Agent) as sweep:
             Args.replay_buffer_size = [1_000_000]
             Agent.batch_size = [256]
             Agent.lr = [1e-4]
+        Args.seed = [100, 200, 300]
 
 
 sweep.save(f"{Path(__file__).stem}.jsonl")
